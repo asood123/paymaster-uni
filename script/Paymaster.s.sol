16197fc99eb7f4a72636a96b7909c84c8b183863
Aseem Sood <asood123@yahoo.com>
2023-10-24 11:56:48 -0400

    checking in current changes to install

--- script/Paymaster.s.sol
+++ script/Paymaster.s.sol
@@ -2,11 +2,41 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
+import '../src/PaymasterAcceptAll.sol';
 
+// deploy command
+// forge script script/Paymaster.s.sol --rpc-url $SEPOLIA_RPC_URL --broadcast --etherscan-api-key $ETHERSCAN_API_KEY --verify -vvvv
 contract PaymasterScript is Script {
-    function setUp() public {}
 
-    function run() public {
-        vm.broadcast();
+
+    function deploy() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        IEntryPoint entryPoint = IEntryPoint(address(vm.envAddress("ENTRY_POINT")));
+        PaymasterAcceptAll paymasterAcceptAll = new PaymasterAcceptAll(entryPoint);
+        paymasterAcceptAll.deposit{value: 1_000_000_000_000_000}();
+
+        vm.stopBroadcast();
+    }
+
+    function deposit() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+        // IEntryPoint entryPoint = IEntryPoint(address(vm.envAddress("ENTRY_POINT")));
+        PaymasterAcceptAll paymasterAcceptAll = PaymasterAcceptAll(address(vm.envAddress("PAYMASTER_ACCEPT_ALL")));
+        paymasterAcceptAll.deposit{value: 1_000_000_000_000_000}();
+        vm.stopBroadcast();
+    }
+
+    function transferToPaymaster() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+        (bool ok, ) = payable(address(vm.envAddress("PAYMASTER_ACCEPT_ALL"))).call{value: 1_000_000_000_000_000}("");
+        require(ok, "send ETH failed");
+        vm.stopBroadcast();
+    }
+    function run() external {
+        this.transferToPaymaster();
     }
 }
